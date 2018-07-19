202b9058569047e68e6cf238f92c2be04e65d169
Todd Fisher <todd.k.fisher@outlook.com>
2018-07-18 20:43:19 -0700

    stuff

--- init.el
+++ init.el
@@ -1,4 +1,11 @@
-﻿(when (>= emacs-major-version 24)
+﻿
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(when (>= emacs-major-version 24)
   (require 'package)
   (setq package-archives '(("gnu"       . "http://elpa.gnu.org/packages/")
                            ;("marmalade" . "http://marmalade-repo.org/packages/")
